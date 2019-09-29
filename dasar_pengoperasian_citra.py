d99c75a9659354dfefae71df9f2219444c6571d0
M. Imam Pratama <imambungo@gmail.com>
2019-09-29 11:45:51 +0700

    operasi biner pada citra grayscale

--- dasar_pengoperasian_citra.py
+++ dasar_pengoperasian_citra.py
@@ -4,5 +4,25 @@ CITRA = Image.open('gambar1.jpg')
 PIXEL = CITRA.load()
 
 print(CITRA.size)
-print(CITRA.size[0])  # ukuran horizontal
-print(CITRA.size[1])  # ukuran vertikal
+ukuran_horizontal = CITRA.size[0]
+ukuran_vertikal = CITRA.size[1]
+print(ukuran_horizontal)
+print(ukuran_vertikal)
+
+# konversi gambar RGB ke grayscale
+# https://stackoverflow.com/a/18778280/9157799
+CITRA_GRAYSCALE = Image.open('gambar1.jpg').convert('L')
+
+# save gambar hasil konversi
+CITRA_GRAYSCALE.save('gambar2.jpg')
+
+PIXEL_GRAYSCALE = CITRA_GRAYSCALE.load()
+
+for x in range(ukuran_horizontal):
+    for y in range(ukuran_vertikal):
+        if PIXEL_GRAYSCALE[x, y] < 128:
+            PIXEL_GRAYSCALE[x, y] = 0
+        else:
+            PIXEL_GRAYSCALE[x, y] = 255
+
+CITRA_GRAYSCALE.save('gambar3.jpg')
