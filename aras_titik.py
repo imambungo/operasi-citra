838029d1f507f0db99632bd601be8e29d6553ae2
M. Imam Pratama <imambungo@gmail.com>
2019-09-29 10:51:39 +0700

    tambahkan source code cara mengakses aras titik

---
+++ aras_titik.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+CITRA = Image.open('gambar1.jpg')
+PIXEL = CITRA.load()
+
+# cara mengakses aras titik
+x = 3
+y = 8
+print(PIXEL[x, y])   # nilai RGB dari pixel (x, y)
+print(PIXEL[x, y][0])  # nilai R dari pixel (x, y)
+print(PIXEL[x, y][1])  # nilai G dari pixel (x, y)
+print(PIXEL[x, y][2])  # nilai B dari pixel (x, y)
