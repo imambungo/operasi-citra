07cb3e1d476958630da04adf84c6ce74af5a3790
defandi17 <55933413+defrianafandi@users.noreply.github.com>
2019-09-29 12:37:21 +0700

    Tambah source code Citra Negatif

---
+++ Citra_Negatif.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+# konversi gambar RGB ke Negatif
+
+CITRA = Image.open('gambar.jpg')
+
+ukuran_horizontal = CITRA.size[0]
+ukuran_vertikal = CITRA.size[1]
+
+PIXEL = CITRA.load()
+
+for x in range(ukuran_horizontal):
+    for y in range(ukuran_vertikal):
+        R = 255 - PIXEL[x, y][0]
+        G = 255 - PIXEL[x, y][1]
+        B = 255 - PIXEL[x, y][2]
+        PIXEL[x, y] = (R, G, B)
+
+CITRA.save('gambar_negatif.jpg')
+
