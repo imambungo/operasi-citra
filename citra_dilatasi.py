5ee53a293368d3da0df2a2165482ae230e7e55f1
defandi17 <55933413+defrianafandi@users.noreply.github.com>
2019-09-29 21:26:44 +0700

    tambah source code operasi dilatasi citra

---
+++ citra_dilatasi.py
@@ -0,0 +1,27 @@
+from PIL import Image
+from math import floor
+
+def skala(s):
+    CITRA = Image.open('gambar.jpg')
+    PIXEL = CITRA.load()
+
+    ukuran_horizontal = CITRA.size[0]
+    ukuran_vertikal = CITRA.size[1]
+
+    ukuran_horizontal_baru = floor(ukuran_horizontal * s)
+    ukuran_vertikal_baru = floor(ukuran_vertikal * s)
+    
+    CITRA_BARU = Image.new("RGB", (ukuran_horizontal_baru, ukuran_vertikal_baru))
+    PIXEL_BARU = CITRA_BARU.load()
+
+    for x in range(ukuran_horizontal_baru):
+        for y in range(ukuran_vertikal_baru):
+            x_lama = ukuran_horizontal * x / ukuran_horizontal_baru
+            y_lama = ukuran_vertikal * y / ukuran_vertikal_baru
+            PIXEL_BARU[x, y] = PIXEL[x_lama, y_lama]
+
+    nama_setelah_disave = 'gambar_dilatasi_skala_' + str(s).replace('.', ',') +'.jpg'
+    CITRA_BARU.save(nama_setelah_disave)
+
+skala(0.5)
+skala(2)
